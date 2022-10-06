761cd5771563b709e1409d857cf2f2e34ad999a7
hltd39 <113762564+hltd39@users.noreply.github.com>
2022-10-06 10:49:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function fruitProcessor(apples, oranges) {
+  const a = `${apples} ${oranges}`;
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+//
+console.log(fruitProcessor(2, 4));
